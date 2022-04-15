6d041a8332fbbfd3baddef7eee5ac04f8bc68510
mocha-maker <flynn.pyc@gmail.com>
2022-04-15 14:21:01 -0400

    Create feedback data, card components, delete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import {useState} from 'react'
 import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import data from './data/FeedbackData'
 
 export default function App() {
+
+  const [feedback, setFeedback] = useState(data)
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete this feedback?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+      }
+    }
+
   return (
     <>
-    <Header text="Feedback"/>
+    <Header />
     <div className="container">
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
     </>
   )
