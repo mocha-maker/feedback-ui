146c8f083ba05343f26ff11c5b3fcf4e584b7bb1
mocha-maker <flynn.pyc@gmail.com>
2022-04-15 15:02:35 -0400

    Complete Feedback Submission Form Functionality

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import FeedbackList from './components/FeedbackList'
 import data from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import { v4 as uuidv4} from 'uuid'
 
 export default function App() {
 
-  const [feedback, setFeedback] = useState(data)
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete this feedback?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+    const [feedback, setFeedback] = useState(data)
+
+    const addFeedback = (newFeedback) => {
+      newFeedback.id = uuidv4()
+      setFeedback([newFeedback, ...feedback])
+    }
+
+    const deleteFeedback = (id) => {
+      if(window.confirm('Are you sure you want to delete this feedback?')) {
+        setFeedback(feedback.filter((item) => item.id !== id))
       }
     }
 
-  return (
-    <>
-    <Header />
-    <div className="container">
-      <FeedbackForm />
-      <FeedbackStats feedback={feedback}/>
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-    </div>
-    </>
-  )
+    return (
+      <>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback}/>
+        <FeedbackStats feedback={feedback}/>
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
+      </div>
+      </>
+    )
 }
--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,23 +1,55 @@
 import {useState} from 'react'
 import Card from './shared/Card'
+import Button from './shared/Button'
+import RatingSelector from './RatingSelector'
 
-function FeedbackForm() {
+function FeedbackForm({handleAdd}) {
 
     const [text, setText] = useState('')
+    const [rating, setRating] = useState(10)
+
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [msg, setMsg] = useState('')
 
     const handleTextChange = (e) => {
+        // Check if text is empty
+        if (text === '') {
+            setBtnDisabled(true)
+            setMsg(null)
+        } else if (text !== '' && text.trim().length < 10) {
+            setBtnDisabled(true)
+            setMsg('Review must be at least 10 characters')
+        } else {
+            setBtnDisabled(false)
+            setMsg(null)
+        }
         setText(e.target.value)
+
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+
+            handleAdd(newFeedback);
+        }
     }
 
     return (
         <Card>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>How would you rate your service with us?</h2>
-                {/* @todo - rating select component */}
+                <RatingSelector select={(rating) => setRating(rating)}/>
                 <div className="input-group">
                     <input onChange={handleTextChange} type="text" value={text} placeholder="Write a Review" />
-                    <button type="submit" className='btn btn-secondary'>Submit</button>
+                    <Button type="submit" isDisabled={btnDisabled}>Submit</Button>
                 </div>
+
+                { msg && <div className='message'>{msg}</div>}
             </form>
         </Card>  
     )
